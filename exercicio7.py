3cce70ee3edcafd22b4af8fe092b955b98fbaef4
William Matheus <willliammatheus947@gmail.com>
2023-07-10 13:08:20 -0300

    Adicionando o arquivo exercicio7

---
+++ exercicio7.py
@@ -0,0 +1,8 @@
+#7) Faça um Programa que calcule a área de um quadrado, em seguida mostre o dobro desta área para o usuário.
+
+l = float(input('Digite o valor do lado do quadrado:'))
+
+a = (l*l)
+dobro = (2*a)
+
+print('O valor dobrado da área do quadrado é :', dobro)
