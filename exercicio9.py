d226955212e4971fc05b79508a7f4738e229223d
William Matheus <willliammatheus947@gmail.com>
2023-07-10 13:21:24 -0300

    Adicionando o arquivo exercicio9

---
+++ exercicio9.py
@@ -0,0 +1,7 @@
+#9) Faça um Programa que peça a temperatura em graus Fahrenheit, transforme e mostre a temperatura em graus Celsius.
+
+f = float(input('Digite o valor da temperatura, em  Fahrenheit :'))
+
+c = 5 * ((f-32) / 9)
+
+print('O valor da temperatura, em graus Celsius, é :', c)
