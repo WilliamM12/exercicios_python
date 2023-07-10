fa62acf5ea982c959aba351ce65624376df8307b
William Matheus <willliammatheus947@gmail.com>
2023-07-10 12:47:10 -0300

    Adicionando o arquivo exercicio3

---
+++ exercicio3.py
@@ -0,0 +1,9 @@
+# 3) Faça um Programa que peça dois números e imprima a soma.
+
+a = int(input('Digite um número:'))
+b = int(input('Digite um número:'))
+
+
+soma = (a + b)
+
+print('A soma dos dois números é:',soma)
