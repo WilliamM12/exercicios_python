48b943df740cdedee197448daae39b8c326a3336
William Matheus <willliammatheus947@gmail.com>
2023-07-10 12:49:54 -0300

    Adicionando o arquivo exercicio4

---
+++ exercicio4.py
@@ -0,0 +1,18 @@
+# 4) Faça um Programa que peça as 4 notas bimestrais e mostre a média.
+
+
+a = int(input('Digite a primeira nota:'))
+b = int(input('Digite a segunda nota:'))
+c = int(input('Digite a terceira nota:'))
+d = int(input('Digite a quarta nota:'))
+
+
+soma = (a + b)
+media = (soma * 0.25)
+
+print('A a média das notas é:', media)
+
+
+
+
+soma = (a + b)
