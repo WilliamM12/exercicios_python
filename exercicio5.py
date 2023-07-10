7dce07f95df103ce2e76ee906a0a0067bbddb1eb
William Matheus <willliammatheus947@gmail.com>
2023-07-10 12:53:29 -0300

    Adicionando o arquivo exercicio5

---
+++ exercicio5.py
@@ -0,0 +1,8 @@
+# 5) Faça um Programa que converta metros para centímetros.
+
+a = int(input('Digite um valor, em metros:'))
+print(a,'m')
+
+conversao = (a * 100)
+
+print('O valor de a, em metros é :', conversao,'cm')
